eeada710f3efc1b32e4ae588872e6793b52de583
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-03-06 22:40:29 -0700

    Add exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print "Enter store name: "
+store_name = gets.chomp
+
+@store7 = Store.create(name: store_name)
+
+puts @store7.errors.full_messages
