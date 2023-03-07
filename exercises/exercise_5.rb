3398844999b9d3d90be8c1b3853bf28165da596d
Xuan Duong Ngo <ngoxd97@gmail.com>
2023-03-06 21:51:14 -0700

    Add exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Sum: #{Store.sum(:annual_revenue)}"
+puts "Average: #{Store.average(:annual_revenue)}"
+puts "Count: #{Store.where(annual_revenue: 1000000..).size}"
